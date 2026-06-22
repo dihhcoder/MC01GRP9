fd3a0a99d36a52e287af392f33ddd3c2ab152c05
alekekex <alexander_khan@dlsu.edu.ph>
2026-06-22 14:38:16 +0800

    added more error handling

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.ArrayDeque;
@@ -11,7 +12,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Reader reader = new Reader();
         boolean isRunning = true;
-        boolean isRead = false;
         int choice, choice2, choice3;
         ArrayList<Map> map;
         ArrayList<Map> routes;
@@ -32,147 +32,174 @@ public class Main {
             System.out.println("+-----------------------------+");
             System.out.println("|   Welcome to Mailman Sims   |");
             System.out.println("+-----------------------------+");
-            System.out.println("Please select an option:");
-            System.out.println("1. Start Simulation");
-            System.out.println("2. Exit");
-            choice = scanner.nextInt();
-            scanner.nextLine();
 
-            switch(choice) {
-                case 1:
-                    System.out.println("Starting the simulation...");
+            try {
+                System.out.println("Please select an option:");
+                System.out.println("1. Start Simulation");
+                System.out.println("2. Exit");
+                choice = scanner.nextInt();
+                scanner.nextLine();
 
-                    do {
-                        map = new ArrayList<Map>();
-                        routes = new ArrayList<Map>();
-                        cities.clear();
-
-                        while(!isRead) {
-                            try {
-                                System.out.print("Enter location of map: ");
-                                filePath = scanner.nextLine();
-                                map = reader.readFile(filePath);
-                                isRead = true;
-                            }
-                            catch(FileNotFoundException e) {
-                                System.out.println("Could not locate file. Please try again.");
-                            }
-                            catch(NumberFormatException e) {
-                                System.out.println("Could not read number format. Please try again.");
-                            }
-                            catch(IOException e) {
-                                System.out.println("Could not read file. Please try again.");
-                            }
-                        }
-
-                        System.out.println("List of cities:");
-                        prevCity = "";
-                        cityIdx = 1;
-                        postOffices = new ArrayList<String>();
-                        cityNames = new ArrayList<String>();
-
-                        for(i = 0; i < map.size(); i++) {
-                            String currCity = map.get(i).getCity();
-                            String postOffice = map.get(i).getOffice();
-
-                            if(!(currCity.equals(prevCity))) {
-                                System.out.println(cityIdx + " - " + currCity);
-                                postOffices.add(postOffice);
-                                cityNames.add(currCity);
-                                cityIdx++;
-                                cityToPost.put(currCity, postOffice);
-                            }
-                            prevCity = currCity;
-                        }
+                switch (choice) {
+                    case 1:
+                        System.out.println("Starting the simulation...");
 
                         do {
-                            System.out.print("Select the number of the city to start: ");
-                            choice2 = scanner.nextInt();
-                            scanner.nextLine();
+                            map = new ArrayList<Map>();
+                            routes = new ArrayList<Map>();
+                            cities.clear();
+                            boolean isRead = false;
 
-                            if(choice2 <= 0 || choice2 >= cityIdx)
-                                System.out.println("Invalid option. Please try again later.");
-                        } while(choice2 <= 0 || choice2 >= cityIdx);
-
-                        cities.add(cityNames.get(choice2 - 1));
-
-                        while(!cities.isEmpty()) {
-
-                            city = cities.peek();
-                            currOffice = cityToPost.get(city);
-                            System.out.println("We are going to " + currOffice + " to deliver the mails.\n");
-                            // (OPTIONAL) ADD A DISPLAY HERE
-                            System.out.print("Enter the amount of mails: ");
-                            mailCount = scanner.nextInt();
-                            scanner.nextLine();
-
-                            for(i = 0; i < mailCount; i++) {
-                                boolean isFound;
-
-                                do {
-                                    isFound = false;
-                                    System.out.print("Destination of Mail " + (i + 1) + ": ");
-                                    destination = scanner.nextLine();
-
-                                    for(j = 0; j < map.size(); j++) {
-                                        if (destination.equals(map.get(j).getUniversity()) && !isFound) {
-                                            routes.add(map.get(j));
-                                            isFound = true;
-
-                                            if(!cities.contains(map.get(j).getCity()) && !map.get(j).getCity().equals(city)) {
-                                                cities.add(map.get(j).getCity());
-                                            }
-                                        }
-                                    }
-                                    if(!isFound)
-                                        System.out.println("Invalid input. Please try again.");
-                                } while(!isFound);
-                            }
-
-                            city = cities.poll();
-                            Sorter.cocktailSort(routes);
-                            System.out.println("Delivering mails to " + city + "...");
-
-                            for(i = 0; i < routes.size(); i++) {   
-                                if(routes.get(i).getCity().equals(city)) {
-                                    System.out.println("Delivering mail to " + routes.get(i).getUniversity() + " in " + routes.get(i).getCity() + " with distance " + routes.get(i).getDistance() + " km");
-                                    // SIMULATION DISPLAY SEQUENCE
-                                    routes.remove(i);
-                                    i--;
+                            while (!isRead) {
+                                try {
+                                    System.out.print("Enter location of map: ");
+                                    filePath = scanner.nextLine();
+                                    map = reader.readFile(filePath);
+                                    isRead = true;
+                                } catch (FileNotFoundException e) {
+                                    System.out.println("Could not locate file. Please try again.");
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Could not read number format. Please try again.");
+                                } catch (IOException e) {
+                                    System.out.println("Could not read file. Please try again.");
                                 }
                             }
 
-                            System.out.println("\nAll mails for " + city + " have been delivered!");
+                            System.out.println("List of cities:");
+                            prevCity = "";
+                            cityIdx = 1;
+                            postOffices = new ArrayList<String>();
+                            cityNames = new ArrayList<String>();
 
-                            if(!cities.isEmpty())
-                                System.out.println("Let us go to the next post office.\n");
-                            else System.out.println("We are officially done for today!\n");
-                        }
+                            for (i = 0; i < map.size(); i++) {
+                                String currCity = map.get(i).getCity();
+                                String postOffice = map.get(i).getOffice();
 
-                        do {
-                            System.out.print("Would you like to simulate again? (1 for Yes, 2 for No): ");
-                            choice3 = scanner.nextInt();
-                            scanner.nextLine();
-
-                            switch(choice3) {
-                                case 1:
-                                    System.out.println("Restarting the simulation...");
-                                    break;
-                                case 2:
-                                    System.out.println("Going Back To The Main Menu...");
-                                    break;
-                                default:
-                                    System.out.println("Invalid option. Please try again.");
+                                if (!(currCity.equals(prevCity))) {
+                                    System.out.println(cityIdx + " - " + currCity);
+                                    postOffices.add(postOffice);
+                                    cityNames.add(currCity);
+                                    cityIdx++;
+                                    cityToPost.put(currCity, postOffice);
+                                }
+                                prevCity = currCity;
                             }
-                        } while(choice3 != 1 && choice3 != 2);
-                    } while(choice3 == 1);
-                    break;
-                case 2:
-                    isRunning = false;
-                    System.out.println("Exiting the program. Goodbye!");
-                    break;
-                default:
-                    System.out.println("Invalid option. Please try again.");
+
+                            boolean isValidNum = false;
+                            choice2 = 0;
+
+                            do {
+                                try {
+                                    System.out.print("Select the number of the city to start: ");
+                                    choice2 = scanner.nextInt();
+                                    scanner.nextLine();
+
+                                    if (choice2 <= 0 || choice2 >= cityIdx)
+                                        System.out.println("Invalid option. Please try again later.");
+                                    else isValidNum = true;
+                                } catch(InputMismatchException e) {
+                                    System.out.println("Invalid input. Please try again.");
+                                    scanner.nextLine();
+                                }
+                            } while (!isValidNum);
+
+                            cities.add(cityNames.get(choice2 - 1));
+
+                            while (!cities.isEmpty()) {
+                                boolean isValidCount = false;
+                                mailCount = 0;
+                                city = cities.peek();
+                                currOffice = cityToPost.get(city);
+                                System.out.println("We are going to " + currOffice + " to get the mails for delivery.\n");
+                                // (OPTIONAL) ADD A DISPLAY HERE
+
+                                do {
+                                    try {
+                                        System.out.print("Enter the amount of mails: ");
+                                        mailCount = scanner.nextInt();
+                                        scanner.nextLine();
+
+                                        if(mailCount < 0)
+                                            System.out.println("Invalid input. Please try again.");
+                                        else isValidCount = true;
+                                    } catch(InputMismatchException e) {
+                                        System.out.println("Invalid input. Please try again.");
+                                        scanner.nextLine();
+                                    }
+                                } while(!isValidCount);
+
+                                for (i = 0; i < mailCount; i++) {
+                                    boolean isFound;
+
+                                    do {
+                                        isFound = false;
+                                        System.out.print("Destination of Mail " + (i + 1) + ": ");
+                                        destination = scanner.nextLine();
+
+                                        for (j = 0; j < map.size(); j++) {
+                                            if (destination.equals(map.get(j).getUniversity()) && !isFound) {
+                                                routes.add(map.get(j));
+                                                isFound = true;
+
+                                                if (!cities.contains(map.get(j).getCity()) && !map.get(j).getCity().equals(city)) {
+                                                    cities.add(map.get(j).getCity());
+                                                }
+                                            }
+                                        }
+
+                                        if (!isFound)
+                                            System.out.println("Invalid input. Please try again.");
+                                    } while (!isFound);
+                                }
+
+                                city = cities.poll();
+                                Sorter.cocktailSort(routes);
+                                System.out.println("Delivering mails to " + city + "...");
+
+                                for (i = 0; i < routes.size(); i++) {
+                                    if (routes.get(i).getCity().equals(city)) {
+                                        System.out.println("Delivering mail to " + routes.get(i).getUniversity() + " in " + routes.get(i).getCity() + " with distance " + routes.get(i).getDistance() + " km");
+                                        // SIMULATION DISPLAY SEQUENCE
+                                        routes.remove(i);
+                                        i--;
+                                    }
+                                }
+
+                                System.out.println("\nAll mails for " + city + " have been delivered!");
+
+                                if (!cities.isEmpty())
+                                    System.out.println("Let us go to the next post office.\n");
+                                else System.out.println("We are officially done for today!\n");
+                            }
+
+                            do {
+                                System.out.print("Would you like to simulate again? (1 for Yes, 2 for No): ");
+                                choice3 = scanner.nextInt();
+                                scanner.nextLine();
+
+                                switch (choice3) {
+                                    case 1:
+                                        System.out.println("Restarting the simulation...");
+                                        break;
+                                    case 2:
+                                        System.out.println("Going Back To The Main Menu...");
+                                        break;
+                                    default:
+                                        System.out.println("Invalid option. Please try again.");
+                                }
+                            } while (choice3 != 1 && choice3 != 2);
+                        } while (choice3 == 1);
+                        break;
+                    case 2:
+                        isRunning = false;
+                        System.out.println("Exiting the program. Goodbye!");
+                        break;
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                }
+            } catch(InputMismatchException e) {
+                System.out.println("Invalid option. Please try again.");
+                scanner.nextLine();
             }
         }
         scanner.close();
