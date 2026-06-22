bbd56ba2f340fdcf8111e1488beab3beec9fb1cd
MarkCasao <mark_anthony_casao@dlsu.edu.ph>
2026-06-22 18:30:59 +0800

    Display Mail storage, route, and animation of delivery

---
+++ Display.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+public class Display {
+	
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
+  public static void showStorage(ArrayList<Map> routes) {
+    System.out.println();
+    System.out.println("========== MAIL STORAGE ==========");
+
+     for(int i = routes.size() - 1; i >= 0; i--) {
+        System.out.println("+----------------------+");
+        System.out.printf("| %-20s |\n", shorten(routes.get(i).getUniversity()));
+      }
+        
+        if(routes.isEmpty())
+			      System.out.println("[ Mail Storage Empty ]");	
+        else 
+            System.out.println("+----------------------+");
+    
+        System.out.println();
+    }
+    
+    public static void showRoute(ArrayList<Map> routes) {
+		System.out.println();
+		System.out.println("========== DELIVERY ROUTE ==========");
+
+		System.out.print("Post Office");
+
+		for(int i = 0; i < routes.size(); i++) {
+			System.out.print(" ---> " + shorten(routes.get(i).getUniversity()));
+		}
+
+		System.out.println();
+		System.out.println();
+	}
+	
+	public static void animateDelivery(String destination) {
+
+		int length = 20;
+
+		for(int i = 0; i <= length; i++) {
+			StringBuilder line = new StringBuilder();
+
+			for(int j = 0; j < i; j++) {
+				line.append("-");
+			}
+
+			line.append("[]");
+
+			for(int j = i; j < length; j++) {
+				line.append("-");
+			}
+
+			line.append("> ");
+			line.append(shorten(destination));
+			System.out.print("\r" + line);
+
+			try {
+				Thread.sleep(100);
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("     Delivered");
+	}
+    
+    private static String shorten(String text) {
+		int maxLength = 20;
+		if(text.length() <= maxLength)
+			return text;
+
+		return text.substring(0, maxLength - 3) + "...";
+	}	
+}
